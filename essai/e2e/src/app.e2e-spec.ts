0f326165bbc48d8e086ac024e2c348d543c9f938
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-08 18:10:49 +0200

    ng e2e

--- essai/e2e/src/app.e2e-spec.ts
+++ essai/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to essai!');
+    expect(page.getTitleText()).toEqual('Hello Jean-Louis GUENEGO!');
   });
 
   afterEach(async () => {
