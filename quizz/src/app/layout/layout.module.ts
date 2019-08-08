7b4ad2606dc02633ae2e597ea1046aefca267c6c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-08 19:00:21 +0200

    create all components

--- quizz/src/app/layout/layout.module.ts
+++ quizz/src/app/layout/layout.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, BodyComponent],
   imports: [
     CommonModule
   ],
-  exports: [LayoutComponent]
+  exports: [LayoutComponent, HeaderComponent, FooterComponent, BodyComponent]
 })
 export class LayoutModule { }
