12c88abf56727df675530012d3b06bd9db4cab34
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-13 18:10:45 +0200

    setup dom

--- quizz/src/app/quizz-create/setup/setup.component.ts
+++ quizz/src/app/quizz-create/setup/setup.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetupComponent implements OnInit {
 
+  name = 'Les capitales d\'europe';
+  nbr = 0;
+
   constructor() { }
 
   ngOnInit() {
