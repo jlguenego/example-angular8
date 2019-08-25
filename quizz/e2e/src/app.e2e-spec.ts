15eed672bdeaa320cc39424b561fa0a1b3daa6e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-25 15:49:59 +0200

    fix e2e home scenario

--- quizz/e2e/src/app.e2e-spec.ts
+++ quizz/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Hello Jean-Louis GUENEGO!');
+    expect(page.getTitleText()).toEqual('Quizz');
   });
 
   afterEach(async () => {
