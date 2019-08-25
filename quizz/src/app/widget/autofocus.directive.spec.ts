e235f0587774478fd93b52c0f74ac15ddceaf8c2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-25 14:44:32 +0200

    test directive autofocus

--- quizz/src/app/widget/autofocus.directive.spec.ts
+++ quizz/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,41 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { Component, DebugElement, OnInit } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+  <div class="toto"></div>
+  <input appAutofocus (focus)="focus()">`
+})
+class TestComponent implements OnInit {
+  isFocused = false;
+  ngOnInit(): void {
+    // console.log('Test NgOnInit');
+  }
+  focus() {
+    // console.log('hey, I am focused');
+    this.isFocused = true;
+  }
+}
 
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofocusDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective]
+    });
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('should create an instance', () => {
+    const directiveElt = fixture.debugElement.query(By.directive(AutofocusDirective));
+    console.log('directiveElt', directiveElt);
+    expect(directiveElt).toBeTruthy();
+    expect(component.isFocused).toBe(true);
+  });
+
 });
