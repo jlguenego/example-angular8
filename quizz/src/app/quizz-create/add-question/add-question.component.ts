57e4662a856c06b5bbdf145094a0707347820f8c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-26 11:50:06 +0200

    add custom validator to add-question

--- quizz/src/app/quizz-create/add-question/add-question.component.ts
+++ quizz/src/app/quizz-create/add-question/add-question.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { QuizzService } from 'src/app/quizz.service';
 import { Question } from 'src/app/question';
 import { Router } from '@angular/router';
+import { CustomValidators } from 'src/app/widget/custom-validators';
 
 @Component({
   selector: 'app-add-question',
@@ -12,11 +13,11 @@ import { Router } from '@angular/router';
 export class AddQuestionComponent implements OnInit {
 
   f = new FormGroup({
-    label: new FormControl('Quelle est la capitale de la France ?', Validators.required),
-    answerA: new FormControl('Paris', Validators.required),
-    answerB: new FormControl('Londres', Validators.required),
-    answerC: new FormControl('Berlin', Validators.required),
-    answerD: new FormControl('Madrid', Validators.required),
+    label: new FormControl('Quelle est la capitale de la France ?', [Validators.required, CustomValidators.maxLength(200)]),
+    answerA: new FormControl('Paris', [Validators.required, CustomValidators.maxLength(100)]),
+    answerB: new FormControl('Londres', [Validators.required, CustomValidators.maxLength(100)]),
+    answerC: new FormControl('Berlin', [Validators.required, CustomValidators.maxLength(100)]),
+    answerD: new FormControl('Madrid', [Validators.required, CustomValidators.maxLength(100)]),
     correctAnswer: new FormControl('', Validators.required)
   });
   constructor(private quizz: QuizzService, private router: Router) { }
