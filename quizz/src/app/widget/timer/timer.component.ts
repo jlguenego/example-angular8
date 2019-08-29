82db4d9aa114c55eda74d497c8110f6845138247
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-29 15:02:45 +0200

    bug: protractor and observable

--- quizz/src/app/widget/timer/timer.component.ts
+++ quizz/src/app/widget/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, NgZone } from '@angular/core';
 import { interval } from 'rxjs';
 import { take, startWith, scan } from 'rxjs/operators';
 
@@ -13,7 +13,7 @@ export class TimerComponent implements OnInit {
   remaining: number;
   @Output() dringdring = new EventEmitter();
 
-  constructor() { }
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
     this.start();
@@ -21,14 +21,22 @@ export class TimerComponent implements OnInit {
 
   start() {
     // start the timer
-    interval(1000).pipe(startWith(0), take(this.init + 1), scan(acc => acc - 1, this.init + 1)).subscribe(
-      n => {
-        this.remaining = n;
-        if (n === 0) {
-          this.dringdring.emit();
-        }
-      }
-    );
-  }
 
+    // protractor issue (you must run this outside an angular zone)
+    this.ngZone.runOutsideAngular(() => {
+      interval(1000).pipe(startWith(0), take(this.init + 1), scan(acc => acc - 1, this.init + 1)).subscribe(
+        n => {
+          // protractor issue (and you must run this in a zone)
+          this.ngZone.run(() => {
+            this.remaining = n;
+            if (n === 0) {
+              this.dringdring.emit();
+            }
+          });
+
+        }
+      );
+    });
+  }
 }
+
